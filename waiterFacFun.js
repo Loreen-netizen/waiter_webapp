f6e88ff241fab7be20ffde5a72c1f4598b158094
Loreen <loreenmuusha.com>
2020-11-20 16:57:17 +0200

    index page and home route rendering properly

--- waiterFacFun.js
+++ waiterFacFun.js
@@ -71,7 +71,7 @@ let waiterFacFun = function(pool) {
         let daysObj = await daysObject();
         let waiterId = await getNameId(waiterName);
         let userSHifts = await getUserShifts(waiterId);
-        console.log({ userSHifts.length });
+        console.log({ userSHifts });
         if (userSHifts !== []) await pool.query(`DELETE FROM shifts WHERE waiter_id = $1`, [waiterId]);
 
         for (const day of daysObj) {
